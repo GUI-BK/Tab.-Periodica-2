4bd118a242e5719a114ce17d4db839cefa1358e8
Guilherme Kobylansky <06664631075@senacrs.edu.br>
2025-05-23 10:47:29 -0300

    c

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { colecaoElementosExp } from "./dados.js";
-function criarTabPer(elementos){
+function criarTabPer(elemments){
 const table = document.createElement('table');
 table.classList.add('tabela-periodica');
 table.classList.add('col-12');
@@ -9,17 +9,41 @@ for(let i = 1; i <= 10; i++){
     for( let j = 1; j <= 18; j++){
         let col = document.createElement('td');
 
-        const elemment = elementos.find(el => el.linha == i && el.coluna == j);
+        const elemment = elemments.find(el => el.linha == i && el.coluna == j);
         if(elemment){
+            
         col.innerHTML =`
-        <div style="background-color:${elemment.corGrupo || '#FFFFF'};"></div>
+        <div class="elemento col-12" style="background-color:${elemment.corGrupo};">
+            <div class="row" style="height: 25%;">
+                <div class="col-12 d-flex justify-content-end">
+                    <span class"numero_atomico">${elemment.numeroAtomico}</span>
+                </div>
+            </div>
+            <div class="row" style="height: 50%;">
+                <div class="col-12 d-flex justify-content-center">
+                    <strong class="simbolo">${elemment.simbolo}</strong>
+                </div>
+            </div>    
+            <div class="row" style="height: 25%;">
+                <div class="col-12 d-flex justify-content-center">
+                    <span class="massa_atomica">${parseFloat(elemment.massaAtomica).toFixed(2)}</span>
+                </div>
+            </div>    
+        </div>
         `
+        if(elemment.grupo === 'lantanídeo'){
+            const elementos = document.querySelectorAll('.elemento');
+            console.log(elementos);
+            elementos.forEach(elemento => {
+                elemento.classList.add('margin-top');
+            });
+        }
 
         }
         row.appendChild(col);
-    }
+    };
     table.appendChild(row);
-}
+};
     document.body.appendChild(table);
-}
+};
 criarTabPer(colecaoElementosExp);
